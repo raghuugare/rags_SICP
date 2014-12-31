0baae709f8b950f6445856cf0d431071c245de6f
Raghavendra Ugare <raghu.ugare@gmail.com>
2014-12-31 23:32:20 +0530

    Extending/generalising the Newton-Raphson method to compute the n'th root of a number 'n'...

---
+++ nth_root.rkt
@@ -0,0 +1,28 @@
+(define (power x n)
+  (define (power-iter result base power)
+  (if (<= power 0)
+      result
+      (power-iter (* result base) base (- power 1))))
+(power-iter 1 x n))
+
+(define (nth-root n number)
+  
+(define (nth-root-iter guess n number)
+ (if (good-enough? guess n number)
+ guess
+ (nth-root-iter (improve guess n number) n number)))
+  
+(define (good-enough? guess n number)
+ (< (abs (- (power guess n) number)) 0.000001))
+  
+(define (improve guess n number)
+(/ (+ (/ number (power guess (- n 1))) (* (- n 1) guess)) n ))
+(nth-root-iter 1.0 n number))
+
+(nth-root 2 25)
+(nth-root 3 2197)
+(nth-root 5 243)
+(nth-root 5 1419857)
+(nth-root 2 144)
+(nth-root 3 27)
+(nth-root 3 729)
