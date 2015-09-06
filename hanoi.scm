a01aa4604b806d610f5d30422fb6d8b86027f94d
Raghu Ugare <raghu.ugare@gmail.com>
2015-09-07 01:06:17 +0530

    Converting Towers of Hanoi to Scheme from Racket...

---
+++ hanoi.scm
@@ -0,0 +1,14 @@
+(define (hanoi n from to temp)
+  (cond ((= n 0) (newline))
+        (else (hanoi (- n 1) from temp to)
+              (print-move from to)
+              (hanoi (- n 1) temp to from))))
+
+(define (print-move from to)
+  (newline)
+  (display "move top disk from ") (display from)
+  (display " to ")(display to))
+
+(hanoi 4 1 2 3)
+(newline)
+
