2deed16119b55da8e1f8ba6c294c65bdd79b48ec
Raghu Ugare <raghu.ugare@gmail.com>
2015-09-07 17:03:08 +0530

    Convert to Scheme! :)

---
+++ sum.scm
@@ -0,0 +1,42 @@
+(define (sum term a next b)
+  (if (> a b)
+      0
+      (+ (term a)
+         (sum term (next a) next b))))
+
+(define (sum-iter term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+     (iter (next a) (+ result (term a)))))
+  (iter a 0))
+
+(define (inc n) (+ n 1))
+(define (cube x) (* x x x))
+(define (square x) (* x x))
+
+(define (sum-cubes a b)
+  (sum cube a inc b))
+
+(define (same p) p)
+
+(define (sum-integers m n)
+  (sum identity m inc n))
+
+(define (pi-sum a b)
+  (define (pi-term x)
+    (/ 1.0 (* x (+ x 2))))
+  (define (pi-next x)
+    (+ x 4))
+  (sum pi-term a pi-next b))
+
+;; define our own identity procedure! :)
+(define identity
+  (lambda (x)
+    x))
+
+(sum-integers 1 10)
+
+(sum-iter identity  1 inc 10)
+
+(* 8 (pi-sum 1 1000))
