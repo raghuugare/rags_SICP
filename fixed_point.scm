becc3a4bcbb2af83f94e2584e411cd30ba5fc8cf
Raghu Ugare <raghu.ugare@gmail.com>
2015-09-14 23:43:03 +0530

    The Fixed-point function!! :)

---
+++ fixed_point.scm
@@ -0,0 +1,25 @@
+;; Fixed point of a function 'f' about a point 'start'...
+;; ------------------------------------------------------
+
+(define (fixed-point f start)
+  ;; t = tolerance
+  (define t 0.000001)
+  
+  (define (iter old new)
+    (if (close-enough? old new)
+        new
+        (iter new (f new))))
+
+  (define (close-enough? old new)
+    (< (abs (- old new)) t))
+
+  (iter start (f start)))
+
+
+;; ================================================
+;; Some tests below for our Fixed-point function...
+;; ================================================
+
+(fixed-point cos 0) ;; Gives 0.7390855263619245
+(fixed-point sin 0) ;; Gives 0
+(fixed-point sin 1) ;; Gives 0.018169909847253
