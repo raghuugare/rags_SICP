31c7c4a2297f1d522c9576cc6177be923d1b8249
Raghu Ugare <raghu.ugare@gmail.com>
2015-10-19 23:26:22 +0530

    An alternate 'view' of finding square-root, as a kind of doing fixed-point analysis! :)

--- fixed_point.scm
+++ fixed_point.scm
@@ -26,3 +26,6 @@
 (fixed-point cos 0) ;; Gives 0.7390855263619245
 (fixed-point sin 0) ;; Gives 0
 (fixed-point sin 1) ;; Gives 0.018169909847253
+
+;; Now, finding the square-root is nothing but finding
+;; 'fixed-point' of the function 'x'
