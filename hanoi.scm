416f244445ee9810d24e06811ed8162c59611390
Raghu Ugare <raghu.ugare@gmail.com>
2019-05-06 16:58:50 +0530

    Correct missing paren

--- hanoi.scm
+++ hanoi.scm
@@ -1,5 +1,5 @@
 ;; The Towers of Hanoi problem 
-d efine (hanoi n from to temp)
+(define (hanoi n from to temp)
   (cond ((= n 0) (newline))
         (else (hanoi (- n 1) from temp to)
               (print-move from to)
